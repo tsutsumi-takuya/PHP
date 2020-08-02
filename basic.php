4f36f53c6139730af96983d34c28877d8d6f6c8c
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-02 22:10:15 +0000

    practice (8/3)

--- basic.php
+++ basic.php
@@ -7,3 +7,18 @@ echo "Hello PHP";
 echo "Hello PHP";
 // PHPは終了タグを省略するのが一般的である
 // この際は改行はコンソールに反映されない
+?>
+
+<?php
+$name = "Andy"; // 変数を設定'$'を必ず記載
+echo "Hello";
+echo $name; // 変数に代入
+?>
+
+<?php
+$name = "Andy";
+$name2 = "Betty";
+$all_names = $name . $name2;
+echo $all_names;
+?>
+
