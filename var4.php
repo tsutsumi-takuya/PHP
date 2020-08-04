fa04a7bd3989a7e99acd14284f0ca20a15cf4654
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-04 06:56:22 +0000

    practice (8/4)

--- var4.php
+++ var4.php
@@ -1,6 +1,6 @@
 <?php
-$name = "Andy"; // '='は代入演算子
-$Name = "Betty"; // 代入演算子は上書きが行える
+$name = "Andy";		// '='は代入演算子
+$Name = "Betty";	// 代入演算子は上書きが行える
 $NAME = "Carol";
 echo $name;
 echo $Name;
@@ -8,9 +8,16 @@ echo $NAME;
 ?>
 
 <?php
-$name = "Andy";
-echo $name;
-$name = "Betty";
-echo $name;
+$name = "Andy";		// '$name'は'Andy'
+echo $name;			// Andy'
+$name = "Betty";	// '$name'は'Betty'に変更
+echo $name;			// 'Betty'
+?>
+
+<?php
+$name = "Andy"; 	// '$name'は'Andy'
+$age = 20;			// '$age'は'20'
+echo "NAME:".$name;	// '$name'の表示
+echo "AGE:".$age;	// '$age'の表示
 ?>
 
