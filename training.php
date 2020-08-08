5e4723e544490fa55c3fde8c4390048d45462047
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-08 07:42:04 +0000

    practice (8/8)

--- training.php
+++ training.php
@@ -22,3 +22,13 @@ $total = $price * $count * $tax;
 echo $total;
 ?>
 
+<?php
+$names = ["Andy","Betty","Carol"];	// 配列
+echo "Hello ";
+echo $names[0];	// 配列変数の指定([インデックス])
+echo "Hello ";
+echo $names[1];	// 配列変数の指定([インデックス])
+echo "Hello ";
+echo $names[2];	// 配列変数の指定([インデックス])
+?>
+
