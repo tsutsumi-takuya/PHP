8cdb21b72a4ab09f12e4ad5c3038f14efc65aaa1
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-10 07:22:55 +0000

    practice (8/10)

--- training-3.php
+++ training-3.php
@@ -30,7 +30,7 @@ var_dump($student);		// $student変数の表示
 
 <?php
 $students = [ // 添字配列
-	["name" => "Andy", "age" => 20], // 連想配列
+	["name" => "Andy", "age" => 20], // 連想配列(文字列と値で管理)
 	["name" => "Betty", "age" => 19],
 	["name" => "Carol", "age" => 21]
 ];
@@ -47,5 +47,43 @@ for ($i = 0; $i < count($students); $i++) {
 $score = ["English" => 90, "Math" => 88, "Science" => 80];
 $total = $score["English"] + $score["Math"] + $score["Science"];
 echo $total;
+?>
+
+<?php
+$score = ["English" => 90, "Math" => 88, "Science" => 80];
+$total = 0;
+
+foreach ($score as $key => $value) { // foreach ($variable as $key => $value){} ※ $variableに配列データ、$keyと$valueは好きに決めて良い
+	$total = $total + $value;
+}
+echo $total .PHP_EOL;
+?>
+
+<?php
+$scores = [ // 二次元配列(配列の中の要素がまた配列になっている)
+	["English" => 90, "Math" => 88, "Science" => 80], // 連想配列(文字列と値で管理)
+	["English" => 64, "Math" => 72, "Science" => 72],
+	["English" => 90, "Math" => 92, "Science" => 94]
+];
+
+$math_total = 0;
+for ($i = 0; $i < count($scores); $i++) {
+	$score = $scores[$i];
+	$math_total= $math_total + $score["Math"];
+}
+echo $math_total .PHP_EOL;
+?>
+
+<?php
+$countries = [
+	"Japan" => ["Tokyo", "Osaka", "Nagoya"], // 連想配列(文字列と値で管理)
+	"England" => ["London", "Birmingham", "Glasgow"],
+	"France" => ["Paris", "Marseille", "Lyon"]
+];
+
+$cities = $countries["England"];
+for ($i = 0; $i < count($cities); $i++) {
+	echo $cities[$i] .PHP_EOL;
+}
 ?>
 
