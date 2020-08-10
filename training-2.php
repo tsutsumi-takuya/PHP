fdfb1dde626b830e0d46e115622995a5b1480671
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-10 02:04:58 +0000

    practice (8/10)

--- training-2.php
+++ training-2.php
@@ -62,3 +62,93 @@ if(!($dice > 3)){ // 条件の否定(if($dive <= 3)と同義)
 }
 ?>
 
+<?php
+$diceA = 6;		// 整数型の代入
+$diceB = "6";	// 文字列型の代入
+
+if($diceA == $diceB){	// 型の相互変換が行われる
+	echo "Win";
+} else {
+	echo "Lose";
+}
+?>
+
+<?php
+$diceA = 6;		// 整数型の代入
+$diceB = "6";	// 文字列型の代入
+
+if ($diceA === $diceB) { // 型の相互変換が行われない
+	echo "Win";
+} else {
+	echo "Lose";
+}
+?>
+
+<?php
+$diceA = 6;		// 整数型の代入
+$diceB = "6";	// 文字列型の代入
+
+$diceC = (int)$diceB; // データ型の変換(キャスト) ※文字列型から整数型(int)に変換
+
+if ($diceA === $diceC) {
+	echo "Win";
+} else {
+	echo "Lose";
+}
+?>
+
+<?php
+$names = ["Andy","Betty","Carol"];
+
+for ($i = 0; $i < 3; $i++) {
+	if ($names[$i]!= "Andy") { // 配列の要素が"Andy"ではない場合
+		echo "Hello ";
+		echo $names[$i] .PHP_EOL;
+	}
+}
+?>
+
+<?php
+$score = $argv[1];	// "$argv" => コマンドライン引数
+echo $score .PHP_EOL;
+var_dump($argv);
+?>
+
+<?php
+$score = $argv[1];	// "$argv" => コマンドライン引数
+
+if ($score >= 80) {
+	echo "$score : A" .PHP_EOL;
+} else if ($score >= 60) {
+	echo "$score : B" .PHP_EOL;
+} else {
+	echo "$score : C" .PHP_EOL;
+}
+?>
+
+<?php
+$scores = [90,72,58,80];
+
+for ($i = 0; $i < count($scores); $i++) {
+	$score = $scores[$i];
+		if ($score >= 80) {
+			echo "$score : A" .PHP_EOL;
+		} else if ($score >= 60) {
+			echo "$score : B" .PHP_EOL;
+		} else {
+			echo "$score : C" .PHP_EOL;
+		}
+	}
+?>
+
+<?php
+$id = $argv[1];
+$password = $argv[2];
+
+if ($id === "Andy" && $password === "password") {
+	echo "Clear" .PHP_EOL;
+} else {
+	echo "Error" .PHP_EOL;
+}
+?>
+
