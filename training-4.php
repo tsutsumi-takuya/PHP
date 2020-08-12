440d4d2268588519e4854029ede65d97bd81e758
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-12 05:25:47 +0000

    practice (8/12)

--- training-4.php
+++ training-4.php
@@ -18,7 +18,7 @@ echo $oupper_case .PHP_EOL;
 
 <?php
 $str = "Hello World";
-$sub = substr($str, 6); // substr関数(文字列の一部を返す) ※ substr(変数名,開始の位置)
+$sub = substr($str, 6); // substr関数(文字列の一部を返す) ※ substr(文字列,開始の位置)
 echo $sub .PHP_EOL;
 ?>
 
@@ -60,5 +60,29 @@ $array = explode("-", $str);
 for ($i = 0; $i < count($array); $i++) {
 	echo substr($array[$i], 0, 1);
 }
+?>
+
+<?php
+$message = "ハローワールド";
+$length = mb_strlen($message); // マルチバイト文字列関数(カタカナ,ひらがな,漢字) ※mb_strlen関数(文字列)
+echo $length .PHP_EOL;
+?>
+
+<?php
+$array = ["Andy","Betty","Carol"];
+$length = count($array);	// count関数 => 配列の要素数をカウントする関数
+echo $length;
+?>
+
+<?php
+$array = ["Andy","Betty","Carol"];
+$reversed_array = array_reverse($array);	// array_reverse関数 => 配列の要素の順番を逆にする関数
+print_r($reversed_array);	// 配列の出力なのでprint_rでの出力になる
+?>
+
+<?php
+$array = [10,20,30];
+$sum = array_sum($array);	// array_sum関数 => 配列の要素数の合計を出す関数
+echo $sum;
 ?>
 
