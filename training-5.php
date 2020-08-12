cb9369271a58a12fea6fc2d893c09cb0ebc0c720
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-12 06:12:55 +0000

    practice (8/12)

--- training-5.php
+++ training-5.php
@@ -16,3 +16,21 @@ $file = "hello.txt";
 file_put_contents($file, $str, FILE_APPEND);	// FILE_APPEND定数を指定 => ファイルに追記する
 ?>
 
+<?php
+$file = "hello.txt";
+$str = file_get_contents($file);	// file_get_contents関数(変数ファイル名) => ファイルの内容を文字列に読み込む関数
+echo $str;
+?>
+
+<?php
+$file = "hello.txt";
+$array = file($file);	// file関数(変数名) => ファイルの内容を読み込んで配列に格納する関数
+print_r($array);
+?>
+
+<?php
+$file = "hello.txt";
+$array = file($file, FILE_IGNORE_NEW_LINES);	// FILE_IGNORE_NEW_LINES定数を指定 => 各行の終端の改行コードを無視する
+print_r($array);
+?>
+
