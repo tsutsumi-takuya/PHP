a507d7615a41fb4ecccce9f9702cb014f2095732
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-18 06:47:48 +0000

    practice (8/18)

---
+++ a.php
@@ -0,0 +1,13 @@
+<?php
+$title = "A Page";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>PHP sample</title>
+</head>
+<body>
+	<h1><?php echo $title; ?></h1>
+</body>
+</html>
---
+++ web1.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Andy";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>PHP sample</title>
+</head>
+<body>
+	<h1>Hello <?php echo $name; ?></h1>
+</body>
+</html>
