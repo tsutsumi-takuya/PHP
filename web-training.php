48ea8a0764fcfd75303030b8b63fb383ba60118c
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-19 06:04:35 +0000

    practice (8/19)

--- web-training.php
+++ web-training.php
@@ -2,6 +2,15 @@
 $date = date("Y-m-d H:i:s");
 $scores = [90,72,58,80];
 $total = array_sum($scores);
+
+$fp = fopen("score.csv", "r");
+$score = [];
+$line = fgetcsv($fp);
+while ($line != false) {
+	$score[] = $line;
+	$line = fgetcsv($fp);
+}
+fclose($fp);
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +22,31 @@ $total = array_sum($scores);
 	<h3><?php echo $date; ?></h3>
 	<h3>Scores</h3>
 	<hr>
+
 	<ul>
 		<?php for ($i = 0; $i < count($scores); $i++) { ?>
 		<li><?php echo $scores[$i]; ?></li>
 		<?php } ?>
 	</ul>
+
 	<p>Total : <?php echo $total; ?></p>
+
+	<hr>
+	<table border="1">
+		<tr>
+			<th>Subject</th>
+			<th>Name</th>
+			<th>Score</th>
+		</tr>
+
+		<?php for ($i = 0; $i < count($score); $i++) { ?>
+
+		<tr>
+			<td><?php echo $score[$i][0]; ?></td>
+			<td><?php echo $score[$i][1]; ?></td>
+			<td><?php echo $score[$i][2]; ?></td>
+		</tr>
+		<?php } ?>
+	</table>
 </body>
 </html>
