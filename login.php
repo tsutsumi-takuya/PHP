60ac67c6e456a55cb176770b65da1f9c9ef732d2
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-20 08:14:55 +0000

    practice (8/20)

--- login.php
+++ login.php
@@ -18,3 +18,24 @@ if ($id === "Andy" && $password === "secret") {
 	<h1>Login <?php echo $message; ?></h1>
 </body>
 </html>
+
+<?php
+$id = $_GET["id"]; // $_GET変数
+$password = $_GET["password"]; // $_GET変数に"id"や"password"が格納
+
+$message = "NG";
+if ($id === "Andy" && $password === "secret") {
+	$message = "OK";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP sample</title>
+</head>
+<body>
+	<h1>Login <?php echo $message; ?></h1>
+</body>
+</html>
