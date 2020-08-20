142069d93a5f2a9afa9c2a0ca76d5cb18b9c11f1
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-20 08:06:35 +0000

    practice (8/20)

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+$id = $_POST["id"]; // $_POST変数　=> スーパーグローバル変数
+$password = $_POST["password"]; // $_POST変数に"id"や"password"が格納
+
+$message = "NG";
+if ($id === "Andy" && $password === "secret") {
+	$message = "OK";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP sample</title>
+</head>
+<body>
+	<h1>Login <?php echo $message; ?></h1>
+</body>
+</html>
