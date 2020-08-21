ba290c93036467edcfe6bc0c4ae4891fd582ea36
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-21 03:16:56 +0000

    practice (8/21)

--- login.php
+++ login.php
@@ -2,19 +2,9 @@
 $id = $_POST["id"]; // $_GET変数
 $password = $_POST["password"]; // $_GET変数に"id"や"password"が格納
 
-$message = "NG";
 if ($id === "Andy" && $password === "secret") {
-	$message = "OK";
+	header("Location: menu.php");
+} else {
+	header("Location: login.html");
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>PHP sample</title>
-</head>
-<body>
-	<h1>Login <?php echo $message; ?></h1>
-</body>
-</html>
---
+++ menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<title>PHP sample</title>
+</head>
+<body>
+	<h3>Menu</h3>
+	<hr>
+	<ul>
+		<li>Todo</li>
+	</ul>
+</body>
+</html>
