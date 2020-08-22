ce3f919af09de428d12653238ce8d88840f5a481
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-22 06:57:57 +0000

    practice (8/22)

--- logout.php
+++ logout.php
@@ -1,5 +1,15 @@
 <?php
 session_start();
-session_destroy(); // session_destroy関数 => ログアウトした場合、紐付けの解除
+
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) { //ini_get関数 => session内でCookieを使用しているか確認
+	$params = session_get_cookie_params(); // cookieのパラメータを保持
+	setcookie(session_name(), '', time() - 42000,
+		$params["path"], $params["domain"],
+		$params["secure"], $params["httponly"]);
+}
+
+session_destroy(); // session_destroy関数 => ログアウトした場合、紐付けの解除(Cookieは残る)
 header("Location: login.html");
 ?>
