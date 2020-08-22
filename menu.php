6acf926078985b23d03ad662ff9b09cb0c0a9969
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-22 06:21:12 +0000

    practice (8/22)

--- menu.php
+++ menu.php
@@ -1,10 +1,17 @@
+<?php
+session_start();
+$id = $_SESSION["id"];
+$time = $_SESSION["time"];
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 	<title>PHP sample</title>
 </head>
 <body>
-	<h3>Menu</h3>
+	<h3>Menu - Hello <?php echo $id; ?></h3>
+	<h5>Login at <?php echo $time; ?></h5>
 	<hr>
 	<ul>
 		<li>Todo</li>
