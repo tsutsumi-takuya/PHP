8513c8cc1e6519afa5f63089bd439dc2ca11e6c6
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-22 06:25:26 +0000

    practice (8/22)

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ $id = $_POST["id"]; // $_GET変数
 $password = $_POST["password"]; // $_GET変数に"id"や"password"が格納
 
 if ($id === "Andy" && $password === "secret") {
-	session_start(); // session_atart関数 => スーパーグローバル変数
-	$_SESSION["id"]; = $id;
+	session_start(); // session_start関数 => スーパーグローバル変数
+	$_SESSION["id"] = $id;
 	$_SESSION["time"] = date("Y-m-d H:i:s");
 
 	header("Location: menu.php");
