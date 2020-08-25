d9f4d67720ebaa551d601b3750fe2fd3f705a166
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-08-25 06:26:55 +0000

    practice (8/25)

---
+++ practice(progate).php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+</head>
+<body>
+  <?php
+    echo "Hello, world!";
+    echo "<br>";
+    echo 7 * 2;
+    echo "<br>";
+    echo 5+7;
+    echo "<br>";
+    echo 8 % 3;
+    echo "<br>";
+    echo "5+7";
+    echo "<br>";
+
+    $fruit = "りんご";
+    echo $fruit;
+    echo "<br>";
+
+    $sum = 8+9;
+    echo $sum;
+    echo "<br>";
+  ?>
+</body>
+</html>
